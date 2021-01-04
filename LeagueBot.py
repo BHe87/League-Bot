08a578cee73b7ae2aecf8bceff2be1bd3e7c981d
Brandon He <theblazesamurai@gmail.com>
2021-01-04 00:22:49 -0500

    small fixes to previous commit

--- LeagueBot.py
+++ LeagueBot.py
@@ -2,7 +2,7 @@
 import os
 import discord
 import json
-import time
+import datetime
 from boto.s3.connection import S3Connection
 from discord.ext import commands
 from riotwatcher import LolWatcher, ApiError
@@ -13,14 +13,15 @@ from riotwatcher import LolWatcher, ApiError
 # discordToken = os.getenv('discord')
 # riotAPI = os.getenv('riot')
 
+
 # if running Heroku
 s3 = S3Connection(os.environ['discord'], os.environ['riot'])
 discordToken = os.environ['discord']
 riotAPI = os.environ['riot']
 
+
 watcher = LolWatcher(riotAPI)
 region = 'na1'
-
 
 # champion info
 champVersions = watcher.data_dragon.versions_for_region(region)['n']['champion']
@@ -41,13 +42,6 @@ async def on_test(context):
 	await context.send(response)
 
 
-    # "id": "ZdQ0DDubuMSlf01Jsb7pwa90N4wIuWiU0qRSLg2oy7W5UuY",
-    # "accountId": "FzkJaZd8rI8nIjimVR26dMF47ZDFelaC9zENN6VjdTUsZDA",
-    # "puuid": "SWjCRqZ8cUHfHMnS4gonCU9xocBFh5cNcJTej5MokKA_tqsJUxTrR8csvNKL5nCfAtx9E9jiePn-KQ",
-    # "name": "BHe",
-    # "profileIconId": 4693,
-    # "revisionDate": 1609204798000,
-    # "summonerLevel": 144
 @bot.command(name='summonerInfo', help='display useless summoner info')
 async def on_summonerInfo(context, username):
 	rawData = watcher.summoner.by_name(region, username)
@@ -60,7 +54,8 @@ async def on_summonerInfo(context, username):
 async def on_rank(context, *arguments):# * operator allows for a variable # of arguments
 	username = ''
 	for x in arguments:
-		username += x
+		username += x + ' '
+	username = username[:-1]
 
 	summoner = watcher.summoner.by_name(region, username)
 
@@ -80,13 +75,15 @@ async def on_rank(context, *arguments):# * operator allows for a variable # of a
 async def on_championMastery(context, *arguments):
 	username = ''
 	for x in arguments:
-		username += x
+		username += x + ' '
+	username = username[:-1]
 
 	summoner = watcher.summoner.by_name(region, username)
 	mastery = watcher.champion_mastery.by_summoner(region, summoner['id'])
 	sortedMasteryDesc = sorted(mastery, key = lambda i: i['championPoints'], reverse=True)
 	top = sortedMasteryDesc[:5]
 
+	response = ''
 	for champion in top:
 		champId = champion['championId']
 		for c in champions:
@@ -101,9 +98,8 @@ async def on_championMastery(context, *arguments):
 		champion.pop('tokensEarned')
 		champion.pop('summonerId')
 		time = champion['lastPlayTime']
-		champion['lastPlayTime'] = datetime.datetime.fromtimestamp(float(time)/1000).strftime('%Y-%m-%d %H:%M')
-
-	response = json.dumps(top, indent=4)
+		champion['lastPlayTime'] = datetime.datetime.fromtimestamp(float(time)/1000).strftime('%H:%M %m-%d-%Y')
+		response += '\n' + json.dumps(champion, indent=4)
 	
 	await context.send(response)
 
