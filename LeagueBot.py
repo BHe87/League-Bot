1c7a8286444469f0a81fa28f6dd0a65c3335023c
Brandon He <theblazesamurai@gmail.com>
2021-01-04 20:22:33 -0500

    added embeds so output is nicer

--- LeagueBot.py
+++ LeagueBot.py
@@ -8,16 +8,16 @@ from discord.ext import commands
 from riotwatcher import LolWatcher, ApiError
 
 # if running local
-# from dotenv import load_dotenv
-# load_dotenv()
-# discordToken = os.getenv('discord')
-# riotAPI = os.getenv('riot')
+from dotenv import load_dotenv
+load_dotenv()
+discordToken = os.getenv('discord')
+riotAPI = os.getenv('riot')
 
 
 # if running Heroku
-s3 = S3Connection(os.environ['discord'], os.environ['riot'])
-discordToken = os.environ['discord']
-riotAPI = os.environ['riot']
+# s3 = S3Connection(os.environ['discord'], os.environ['riot'])
+# discordToken = os.environ['discord']
+# riotAPI = os.environ['riot']
 
 
 watcher = LolWatcher(riotAPI)
@@ -57,23 +57,50 @@ async def on_summonerInfo(context, *arguments):
 
 @bot.command(name='rank', help='display your rank info')
 async def on_rank(context, *arguments):# * operator allows for a variable # of arguments
+	mode = arguments[-1]
+
 	username = ''
-	for x in arguments:
+	for x in arguments[:-1]:
 		username += x + ' '
-	username = username[:-1]
 
 	summoner = watcher.summoner.by_name(region, username)
 
 	# returns a list where rawData[0] = flex rank, rawData[1] = solo/duo rank
 	rawData = watcher.league.by_summoner(region, summoner['id'])
 	
-	response = ''
-	for queueType in rawData:
-		queueType.pop('leagueId')
-		queueType.pop('summonerId')
-		response += '\n' + json.dumps(queueType, indent=4)
+	embed = discord.Embed(title='Current ' + mode + ' Rank: ', description='')
 
-	await context.send(response)
+	if mode == 'solo':
+		try:
+			rawData = rawData[1]
+		except IndexError:
+			await context.send(username + ' is not ranked in this queue')
+	elif mode == 'flex':
+		try:
+			rawData = rawData[0]
+		except IndexError:
+			await context.send(username + ' is not ranked in this queue')
+
+	rawData.pop('queueType')
+	rawData.pop('leagueId')
+	rawData.pop('summonerId')
+	wins = rawData['wins']
+	losses = rawData['losses']
+	rawData['winrate'] = str(100 * (float)(wins/(wins+losses))) + '%'
+
+	keys = ''
+	for key in rawData.keys():
+		keys += '\n' + key
+
+	values = ''
+	for value in rawData.values():
+		values += '\n' + str(value)
+
+	embed.add_field(name='_', value=keys)
+	embed.add_field(name='_', value=values)
+	embed.set_author(name=username, icon_url='')
+
+	await context.send(embed=embed)
 
 
 @bot.command(name='championMastery', help='display your top 5 champion mastery')
@@ -87,6 +114,8 @@ async def on_championMastery(context, *arguments):
 	mastery = watcher.champion_mastery.by_summoner(region, summoner['id'])
 	sortedMasteryDesc = sorted(mastery, key = lambda i: i['championPoints'], reverse=True)
 	top = sortedMasteryDesc[:5]
+
+	embed = discord.Embed(title='Champion Mastery: ', description='')
 
 	response = ''
 	for champion in top:
@@ -104,9 +133,21 @@ async def on_championMastery(context, *arguments):
 		champion.pop('summonerId')
 		time = champion['lastPlayTime']
 		champion['lastPlayTime'] = datetime.datetime.fromtimestamp(float(time)/1000).strftime('%H:%M %m-%d-%Y')
-		response += '\n' + json.dumps(champion, indent=4)
-	
-	await context.send(response)
+		# response += '\n' + json.dumps(champion, indent=4)
+		keys = ''
+		for key in champion.keys():
+			keys += '\n' + key
+
+		values = ''
+		for value in champion.values():
+			values += '\n' + str(value)
+
+		embed.add_field(name='_', value=keys)
+		embed.add_field(name='_', value=values)
+
+	embed.set_author(name=username, icon_url='')
+
+	await context.send(embed=embed)
 
 
 bot.run(discordToken)
